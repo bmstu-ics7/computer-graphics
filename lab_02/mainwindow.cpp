c3e3a6ab6ae08674a00e17cc80b36fc1dacebb29
Willsem <happymainer132@yandex.ru>
2019-02-27 13:05:24 +0300

    ADD: first picture

--- lab_02/mainwindow.cpp
+++ lab_02/mainwindow.cpp
@@ -12,3 +12,22 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::paintEvent(QPaintEvent *) {
+    QPainter p(this);
+
+    int canvasWidth = width() - 200;
+    int canvasHeight = height();
+
+    p.fillRect(0, 0, canvasWidth, canvasHeight, Qt::white);
+
+    int R = 100;
+    QPair<int, int> center = QPair<int, int>(canvasWidth / 2, canvasHeight / 2);
+
+    p.setPen(QPen(Qt::black, 2, Qt::SolidLine, Qt::FlatCap));
+    p.drawEllipse(center.first - R, center.second - R, R * 2, R * 2);
+
+    p.drawEllipse(center.first - R / 3 - R / 8, center.second - R / 2, R / 3, R / 4);
+    p.drawEllipse(center.first + R / 3 - R / 8, center.second - R / 2, R / 3, R / 4);
+
+}
